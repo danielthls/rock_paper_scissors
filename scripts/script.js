2a17d987be6f68a3f1b27579e0295c71f59624b3
Daniel Thomaz <daniel_thls@yahoo.com.br>
2022-09-07 13:56:00 -0300

    Overhaul of the page structure, add images, get buttons working and colors changing accordingly

--- scripts/script.js
+++ scripts/script.js
@@ -9,15 +9,43 @@ function getComputerChoice() {
     } else {
         computerChoice = 'paper'
     }
+
+    colorButton('.button1',computerChoice);
+    
     return computerChoice
 }
-function playerSelect() {
-    //read player`s input
-    let input = prompt()
-    if (input === null) {
-        input = '';
+
+function colorButton(button,choice) {
+    
+    removeColorButton(button);
+
+    const divSelect = document.querySelectorAll(button);
+
+    for (i=0; i<divSelect.length; i++) {
+        
+        let divButton = divSelect[i];
+        
+        if (divButton.id === choice) {
+            divButton.style.background = 'blue';
+        }
     }
-    let choice = input.toLowerCase();
+    return;
+}
+
+function removeColorButton(button) {
+    const playerSelect1 = document.querySelectorAll(button);
+    for (i=0; i<playerSelect1.length; i++) {
+        let playerButton = playerSelect1[i];
+        playerButton.style.background = 'none';
+    }
+
+    return;
+}
+
+function playerSelect(value) {
+    //read player`s input
+    let choice = value.toLowerCase();
+    colorButton('.button',choice)
     return choice;
 }
 
@@ -26,74 +54,113 @@ function gameResult(playerChoice, computerChoice) {
     //Conditionals include all three possibilities for each possible pick the player chooses.
     let result = '';
     let resultCounter = '';
-
+    let winner = '---';
+    let loser = '---';
 
     if (playerChoice === 'rock') {
-        result = 'You selected Rock!'
+        //result = 'You selected Rock!'
         if (computerChoice === 'rock') {
-            result += ' The computer picked Rock! Draw!'
-            resultCounter = 'draw';
+           winner = 'player';
+           loser = 'player';
+           // result += ' The computer picked Rock! Draw!'
+            //resultCounter = 'draw';
         } else if (computerChoice === 'scissors') {
-            result += ' The computer picked Scissors! Win!'
-            resultCounter = 'win';
+            winner = 'player';
+            loser = 'computer';
+            //result += ' The computer picked Scissors! Win!'
+            //resultCounter = 'win';
         } else if (computerChoice === 'paper') {
-            resultCounter = 'lose';
-            result += ' The computer picked Paper! Lose!'
+           winner = 'computer';
+           loser = 'player';
+            //resultCounter = 'lose';
+            //result += ' The computer picked Paper! Lose!'
         }
     } else if (playerChoice === 'scissors') {
-        result = 'You selected Scissors!'
+        //result = 'You selected Scissors!'
         if (computerChoice === 'rock') {
-            result += ' The computer picked Rock! Lose!'
-            resultCounter = 'lose';
+            winner = 'computer';
+            loser = 'player';
+            //result += ' The computer picked Rock! Lose!'
+            //resultCounter = 'lose';
         } else if (computerChoice === 'scissors') {
-            result += ' The computer picked Scissors! Draw!'
-            resultCounter = 'draw';
+            winner = 'player';
+           loser = 'player';
+            //result += ' The computer picked Scissors! Draw!'
+            //resultCounter = 'draw';
         } else if (computerChoice === 'paper') {
-            result += ' The computer picked Paper! Win!'
-            resultCounter = 'win';
+            winner = 'player';
+           loser = 'computer';
+            //result += ' The computer picked Paper! Win!'
+            //resultCounter = 'win';
         }
     } else if (playerChoice === 'paper') {
-        result = 'You selected Paper!'
+        //result = 'You selected Paper!'
         if (computerChoice === 'rock') {
-            result += ' The computer picked Rock! Win!'
-            resultCounter = 'win'
+            winner = 'player';
+            loser = 'computer';
+            //result += ' The computer picked Rock! Win!'
+           //resultCounter = 'win'
         } else if (computerChoice === 'scissors') {
-            result += ' The computer picked Scissors! Lose!'
-            resultCounter = 'lose'
+            winner = 'computer';
+            loser = 'player';
+            //result += ' The computer picked Scissors! Lose!'
+            //resultCounter = 'lose'
         } else if (computerChoice === 'paper') {
-            result += ' The computer picked Paper! Draw!'
-            resultCounter = 'draw'
+            winner = 'player';
+            loser = 'player';
+            //result += ' The computer picked Paper! Draw!'
+            //resultCounter = 'draw'
         }
-    } else {
-        result = 'Invalid output!'
+    //} else {
+        //result = 'Invalid output!'
     }
-    console.log(result);
-    return resultCounter;
+    console.log(winner, loser);
+    //let green = document.querySelector('.' + CSS.escape(winner) + '-buttons')
+
+    //let red = document.querySelector('.' + CSS.escape(loser) + '-buttons')
+
+    drawBoard(winner, loser);
+
+    return;
+    //console.log(result);
+    //return resultCounter;
+}
+
+function clearBoard() {
+    const playerClear = document.querySelector('.player-buttons');
+    const computerClear =document.querySelector('.computer-buttons');
+    playerClear.style.background = 'none';
+    computerClear.style.background = 'none';
+}
+
+function drawBoard(green,red) {
+    let roundWinner = document.querySelector('.' + CSS.escape(green) + '-buttons');
+    let roundLoser = document.querySelector('.' + CSS.escape(red) + '-buttons');
+    roundWinner.style.background = 'green';
+    roundLoser.style.background = 'red';
+    if (green === red) {
+        roundDraw = document.querySelector('.computer-buttons')
+        roundWinner.style.background = 'yellow';
+        roundDraw.style.background = 'yellow';
+    }
 }
 
 function game() {
-    //add each win, loss, draw or invalid output to one of the elements in the array
-    let result = ''
-    let score = [0,0,0,0];
-    for (i=0; i<5; i++) {
-        //results tracker: [wins, losses, draws, invalid answers]
-        result = gameResult(playerSelect(),getComputerChoice())
-        if (result === 'win') {
-            score[0] ++;
-        } else if (result === 'lose') {
-            score[1] ++;
-        } else if (result === 'draw') {
-            score[2] ++;
-        } else {
-            score[3] ++;
-        }
-    }
-    let message = 'Wins: ' + score[0] 
-    + ', Losses: ' + score[1] 
-    + ', Draws: ' + score[2] 
-    + ', Invalid Outputs: ' + score[3]; 
-    console.log(message)
-    return message;
+    clearResult();
+    clearBoard();
+    currentScore();
 }
 
-game();
+function currentScore() {
+    
+}
+
+const divSelect = document.querySelectorAll('.button');
+
+divSelect.forEach(function(divButton) {
+    divButton.addEventListener("click", function() {
+        gameResult(playerSelect(divButton.id),getComputerChoice())
+    });
+});
+
+
